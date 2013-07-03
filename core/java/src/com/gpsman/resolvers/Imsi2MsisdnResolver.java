5a680d7978755c37e91278babd8d852b042efad2
gokhang <gokhang@telenity.com>
2013-07-03 12:08:26 +0300

    imsi changed to imei

--- core/java/src/com/gpsman/resolvers/Imsi2MsisdnResolver.java
+++
@@ -1,9 +0,0 @@
-package com.gpsman.resolvers;
-
-public interface Imsi2MsisdnResolver {
-
-	public static final String __file_identity = "@(#)$Id:  IMSI2MSISDNResolver.java,v 1.1 Jun 25, 2013 4:45:00 PM gokhang Exp $";
-
-	String getMsisdnForImsi (String imsi);
-
-}
